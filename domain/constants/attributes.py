d230c91756769d3271486ca7f784a5ca0f745568
Kelly (Young) Popko <kelpopko@gmail.com>
2025-04-26 14:05:09 -0500

    Add pet characteristics, attributes

--- domain/constants/attributes.py
+++ domain/constants/attributes.py
@@ -13,16 +13,26 @@ class Size(str, Enum):
 
 
 class Temperament(str, Enum):
-    CALM = "Calm"
-    EXCITABLE = "Excitable"
+    AFFECTIONATE = "Affectionate"
     ANXIOUS = "Anxious"
     ASSERTIVE = "Assertive"
+    ATHLETIC = "Athletic"
+    CALM = "Calm"
+    CURIOUS = "Curious"
+    EXCITABLE = "Excitable"
+    FUNNY = "Funny"
+    HAPPY = "Happy"
+    LOYAL = "Loyal"
+    PLAYFUL = "Playful"
+    QUIET = "Quiet"
+    SMART = "Smart"
 
 
 class FurType(str, Enum):
     SHORT = "Short"
     MEDIUM = "Medium"
     LONG = "Long"
+    CURLY = "Curly"
 
 
 class IntelligenceLevel(str, Enum):
@@ -35,12 +45,23 @@ class MaintenanceLevel(str, Enum):
     LOW = "Low"
     MEDIUM = "Medium"
     HIGH = "High"
-    
+
 class Availability(str,Enum):
-    AVAILABLE = "Available"
-    ADOPTED = "Adopted"
-    UNLISTED = "Unlisted"
-    
+    AVAILABLE = "Available"             # Currently supports adoptable dogs
+    # ADOPTED = "Adopted"               # In the future will support other options
+    # UNLISTED = "Unlisted"
+
 class Sex(str, Enum):
     MALE = "Male"
     FEMALE = "Female"
+
+class Environment(str, Enum):
+    CHILDREN = "Children"
+    DOGS = "Dogs"
+    CATS = "Cats"
+
+class Age(str, Enum):
+    BABY = "Baby"
+    YOUNG = "Young"
+    ADULT = "Adult"
+
