2addac598a91e6611ca21a6ddb348ea4b8c14d0f
Joey <josephrobertlopez@gmail.com>
2025-04-26 11:47:58 -0500

    pet charcteristics

---
+++ domain/constants/attributes.py
@@ -0,0 +1,36 @@
+class ActivityLevel(str, Enum):
+    LOW = "Low"
+    MEDIUM = "Medium"
+    HIGH = "High"
+
+
+class Size(str, Enum):
+    SMALL = "Small"
+    MEDIUM = "Medium"
+    LARGE = "Large"
+
+
+class Temperament(str, Enum):
+    CALM = "Calm"
+    EXCITABLE = "Excitable"
+    ANXIOUS = "Anxious"
+    ASSERTIVE = "Assertive"
+
+
+class FurType(str, Enum):
+    NONE = "None"
+    SHORT = "Short"
+    MEDIUM = "Medium"
+    LONG = "Long"
+
+
+class IntelligenceLevel(str, Enum):
+    LOW = "Low"
+    MEDIUM = "Medium"
+    HIGH = "High"
+
+
+class MaintenanceLevel(str, Enum):
+    LOW = "Low"
+    MEDIUM = "Medium"
+    HIGH = "High"
